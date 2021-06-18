50b5b52cca6b496bbb9ef466e18ec954cba34c34
kameoman <spiderman.935963@gmail.com>
2021-06-18 15:59:29 +0900

    追加

--- 行き先占い/js/main.js
+++ 行き先占い/js/main.js
@@ -4,23 +4,20 @@
   const btn = document.getElementById("btn");
 
   btn.addEventListener("click", () => {
-    // const results = ["大吉", "中吉", "凶", "末吉"];
-    // const results = ["大吉", "大吉", "大吉", "大吉",'凶'];
-    // const n = Math.floor(Math.random() * results.length);
-    // // btn.textContent = results[n];
-    // btn.textContent = results[Math.floor(Math.random() * results.length)];
+    const results = ["大吉", "中吉", "凶", "末吉"];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
 
     // 確率も操作ができる
-    const n = Math.random();
+    // const n = Math.random();
     // 5％の確率で大吉
-    if (n < 0.05){
-      btn,textContent = '大吉';
+    // if (n < 0.05){
+    //   btn,textContent = '大吉';
       // １５％の確率
-    }　else if (n < 0.2) {
-      btn.textContent = '小吉';
-    } else {
-      btn.textContent = '凶';
-    }
+    // }　else if (n < 0.2) {
+    //   btn.textContent = '小吉';
+    // } else {
+    //   btn.textContent = '凶';
+    // }
 
     // btn.textContent =  n;
 
@@ -37,3 +34,36 @@
     // }
   });
 }
+$(function() {
+
+  // button
+  var btn_clone = $('.btn-clone');  // 追加ボタン
+  var btn_remove = $('.btn-remove');  // 削除ボタン
+
+  // clone
+  btn_clone.click(function() {
+
+      var text = $('.text').last();  // 最後尾にあるinput
+
+      text
+        .clone()  // クローン
+        .val('')  // valueもクローンされるので削除する
+        .insertAfter(text);  // inputを最後尾に追加
+
+      if ($('.text').length >= 2) {
+          $(btn_remove).show();  // inputが2つ以上あるときに削除ボタンを表示
+      }
+
+  });
+
+  // remove
+  btn_remove.click(function() {
+
+      $('.text').last().remove();
+
+      if ($('.text').length < 2) {
+          btn_remove.hide();  // inputが2つ未満のときに削除ボタンを非表示
+      }
+
+  });
+});
